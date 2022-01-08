df210daafba0139f70275c3e1ce86a2e47ff5161
NalinGeeganage <nalinruwan1996@gmail.com>
2022-01-08 11:07:57 +0530

    Update basic Application

---
+++ TextEdditor/src/AppInitializer.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("view/mainForm.fxml"));
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simple Text Editor");
+        primaryStage.fullScreenProperty();
+        primaryStage.show();
+
+
+    }
+
+
+}
---
+++ TextEdditor/src/controller/mainFormController.java
@@ -0,0 +1,120 @@
+package controller;
+
+
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+import javafx.stage.FileChooser;
+
+import java.io.*;
+
+
+
+
+public class mainFormController {
+
+    public TextArea txtArea;
+    public MenuItem mnNew;
+    public MenuItem mnItemOpen;
+    public MenuItem mnIItemSave;
+    public MenuItem mnExit;
+    public MenuItem btnSelectAll;
+    public MenuItem btnCopy;
+    public MenuItem btnPaste;
+    public MenuItem btnCut;
+    public Menu btnHelp;
+
+    public void initialize(){
+
+    }
+
+    public void mnItemOpenOnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open a file");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt")
+        );
+        File file = fileChooser.showOpenDialog(null);
+        try{
+            FileInputStream fis = new FileInputStream(file);
+            byte[] fileBytes = new byte[fis.available()];
+
+            fis.read(fileBytes);
+            String fileContent = new String(fileBytes);
+            txtArea.setText(fileContent);
+            fis.close();
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void mnNewClickOnAction(ActionEvent actionEvent) {
+        txtArea.clear();
+    }
+
+    public void mnItemSaveClickOnAction(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser1 = new FileChooser();
+        fileChooser1.setTitle("select a Folder");
+        File file =fileChooser1.showSaveDialog(null);
+
+        byte[] bytes = txtArea.getText().getBytes();
+
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(bytes);
+        fos.close();
+    }
+
+    public void mnExitClockOnAction(ActionEvent actionEvent) {
+
+        System.exit(0);
+    }
+
+    public void btnSelectAllClickOnActions(ActionEvent actionEvent) {
+        txtArea.selectAll();
+
+
+    }
+
+    public void btnCopyClickOnAction(ActionEvent actionEvent) {
+
+        Clipboard systemClipboard = Clipboard.getSystemClipboard();
+        ClipboardContent clipboardContent = new ClipboardContent();
+        clipboardContent.putString(txtArea.getSelectedText());
+        systemClipboard.setContent(clipboardContent);
+    }
+
+    public void btnPasteClickOnAction(ActionEvent actionEvent) {
+        if (txtArea.getSelectedText().isEmpty()){
+            Clipboard systemClipboard = Clipboard.getSystemClipboard();
+            int caretPosition = txtArea.getCaretPosition();
+            txtArea.insertText(caretPosition,systemClipboard.getString());
+        }
+        else{
+            txtArea.replaceSelection("");
+
+            Clipboard systemClipboard = Clipboard.getSystemClipboard();
+            int caretPosition = txtArea.getCaretPosition();
+            txtArea.insertText(caretPosition,systemClipboard.getString());
+
+        }
+    }
+
+    public void btnCutClickOnAction(ActionEvent actionEvent) {
+        Clipboard systemClipboard = Clipboard.getSystemClipboard();
+        ClipboardContent clipboardContent = new ClipboardContent();
+        clipboardContent.putString(txtArea.getSelectedText());
+
+        systemClipboard.setContent(clipboardContent);
+        txtArea.replaceSelection("");
+    }
+
+    public void btnhelpClickOnAction(ActionEvent actionEvent) {
+    }
+}
